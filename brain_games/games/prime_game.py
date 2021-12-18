c43b60573a99d334e51bfb515cb72b44ebc3350d
Solenov <colenov@yandex.ru>
2021-12-18 17:17:15 +0300

    Fix prime_game.py and brain_prime.py

--- brain_games/games/prime_game.py
+++ brain_games/games/prime_game.py
@@ -2,12 +2,10 @@
 
 
 from random import randint
-from brain_games import engine
 
-text_of_rules = 'Answer "yes" if given number is prime. Otherwise answer "no".'
-n = engine.NUMBER_OF_ROUNDS
+TEXT_OF_RULES = 'Answer "yes" if given number is prime. Otherwise answer "no".'
 list_of_prime_numbers = [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37]
-MAX_NUMBER = 40     # Max number in the game.
+max_num = 40     # Max number in the game.
 
 
 def prime_test(number):
@@ -20,23 +18,10 @@ def prime_test(number):
     return(correct_answer)
 
 
-def main(username):
-
-    for i in range(n):
-        task = randint(1, MAX_NUMBER)
-        correct_answer = prime_test(task)
-        engine.ask_user(task)
-        user_answer = engine.get_user_answer()
-        result = engine.check_answer(user_answer, correct_answer)
-
-        if result:
-            engine.next_round()
-            continue
-        else:
-            engine.game_over(username, user_answer, correct_answer)
-            break
-    else:
-        engine.user_win(username)
+def main():
+    task = randint(1, max_num)
+    correct_answer = prime_test(task)
+    return correct_answer, task
 
 
 if __name__ == '__main__':
--- brain_games/scripts/brain_prime.py
+++ brain_games/scripts/brain_prime.py
@@ -1,12 +1,11 @@
 #!/usr/bin/env python
 
-from brain_games import welcome_user
+from brain_games import engine
 from brain_games.games import prime_game
 
 
 def main():
-    name = welcome_user.get_name()
-    prime_game.main(name)
+    engine.run(prime_game)
 
 
 if __name__ == '__main__':
