4c31013a45fb6383978f6f9125c3335de2d76b4f
Solenov <colenov@yandex.ru>
2021-12-28 00:03:50 +0300

    Fix calc_game.py

--- brain_games/games/calc_game.py
+++ brain_games/games/calc_game.py
@@ -8,8 +8,8 @@ DESCRIPTION = 'What is the result of the expression?'
 def generate_round():
     a = randint(1, 10)
     b = randint(1, 10)
-    list_of_operations = [('+', add), ('-', sub), ('*', mul)]
-    operation, function = choice(list_of_operations)
+    operations_and_function = [('+', add), ('-', sub), ('*', mul)]
+    operation, function = choice(operations_and_function)
     question = f'{a} {operation} {b}'
     correct_answer = function(a, b)
     correct_answer = str(correct_answer)
