f958124780ae194dbe368fa9203adb8e88edc8b2
Solenov <colenov@yandex.ru>
2021-12-28 00:06:36 +0300

    Fix engine.py

--- brain_games/engine.py
+++ brain_games/engine.py
@@ -1,11 +1,19 @@
 from brain_games import cli
+import prompt
 
 
 NUMBER_OF_ROUNDS = 3
 
 
+def welcome_user():
+    print('Welcome to the Brain Games!')
+    name = prompt.string('May I have your name? ')
+    print('Hello, ' + name + '!')
+    return name
+
+
 def run(game):
-    username = cli.welcome_user()
+    username = welcome_user()
     print(game.DESCRIPTION)
 
     for i in range(NUMBER_OF_ROUNDS):
